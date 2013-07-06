17a703087adb49315640d991b25da007e5b84d69
Mark Fine <mark.fine@gmail.com>
2013-07-06 17:44:09 -0400

    more refactoring.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"code.google.com/p/go-uuid/uuid"
 	"database/sql"
 	"encoding/json"
 	"flag"
@@ -19,18 +18,19 @@ import (
 )
 
 var (
-	limit     = flag.Int("limit", 1000, "Query Limit")
-	insert  = flag.Bool("insert", false, "Insert Worker")
-	update  = flag.Bool("update", false, "Update Worker")
-	dbUrl = mustGetenv("DATABASE_URL")
-	db    = dbOpen()
-	urlRe = regexp.MustCompile("<(.*)>; rel=\"(.*)\"")
-	org   = mustGetenv("GITHUB_ORG")
-	auth  = "token " + mustGetenv("GITHUB_OAUTH_TOKEN")
-	wg    sync.WaitGroup
+	limit  = flag.Int("limit", 1000, "Query Limit")
+	setup  = flag.Bool("setup", false, "Setup Database")
+	insert = flag.Bool("insert", false, "Insert Worker")
+	update = flag.Bool("update", false, "Update Worker")
+	dbUrl  = mustGetenv("DATABASE_URL")
+	db     = dbOpen()
+	urlRe  = regexp.MustCompile("<(.*)>; rel=\"(.*)\"")
+	org    = mustGetenv("GITHUB_ORG")
+	auth   = "token " + mustGetenv("GITHUB_OAUTH_TOKEN")
+	wg     sync.WaitGroup
 )
 
-type handler func(rc io.ReadCloser) error
+type handler func(rc io.ReadCloser)
 
 func nextUrl(hdr http.Header) string {
 	for _, link := range hdr["Link"] {
@@ -46,154 +46,129 @@ func nextUrl(hdr http.Header) string {
 	return ""
 }
 
-func rateLimit(hdr http.Header) (remaining, reset int, err error) {
-	remaining, err = strconv.Atoi(hdr["X-Ratelimit-Remaining"][0])
+func rateLimit(hdr http.Header) (int, int) {
+	remaining, err := strconv.Atoi(hdr["X-Ratelimit-Remaining"][0])
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 
-	reset, err = strconv.Atoi(hdr["X-Ratelimit-Reset"][0])
+	reset, err := strconv.Atoi(hdr["X-Ratelimit-Reset"][0])
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 
-	return
+	return remaining, reset
 }
 
-func requester(url string, h handler) error {
+func request(url string, h handler) {
 	for url != "" {
 		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
-			return err
+			log.Fatal(err)
 		}
 		req.Header.Set("Authorization", auth)
 
 		resp, err := http.DefaultClient.Do(req)
 		if err != nil {
-			return err
+			log.Fatal(err)
 		}
 
-		if remaining, reset, err := rateLimit(resp.Header); err != nil {
-			log.Fatal(err)
-		} else if remaining == 0 {
-			log.Printf("sleeping reset=%s\n", time.Unix(int64(reset), 0))
+		if remaining, reset := rateLimit(resp.Header); remaining == 0 {
+			log.Printf("reset=%s\n", time.Unix(int64(reset), 0))
 			time.Sleep(time.Unix(int64(reset), 0).Sub(time.Now()))
 			continue
 		}
 
-		if err = h(resp.Body); err != nil {
-			return err
-		}
+		h(resp.Body)
 
 		url = nextUrl(resp.Header)
 	}
-
-	return nil
 }
 
-func shasHandler(id string) handler {
-	return func(rc io.ReadCloser) error {
+func shas(id string) handler {
+	return func(rc io.ReadCloser) {
 		defer rc.Close()
 
 		var result struct {
 			Commit struct {
-				Author struct {
-					Email string
-					Date string
-				}
 				Message string
+				Author  struct {
+					Email string
+					Date  string
+				}
 			}
 			Stats struct {
 				Additions int
 				Deletions int
-				Total int
+				Total     int
 			}
 		}
 
 		if err := json.NewDecoder(rc).Decode(&result); err != nil {
-			return err
+			log.Fatal(err)
 		}
 
-		shasDbUpdate(id,
+		dbUpdate(id,
 			result.Commit.Author.Email,
 			result.Commit.Author.Date,
 			result.Commit.Message,
 			result.Stats.Additions,
 			result.Stats.Deletions,
 			result.Stats.Total)
-
-		return nil
 	}
 }
 
-func shas(id, repo, sha string) {
-	log.Printf("repo=%s sha=%s\n", repo, sha)
-	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/commits/%s", org, repo, sha)
-	if err := requester(url, shasHandler(id)); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func shasDbUpdate(id, email, date, message string, additions, deletions, total int) {
-	rows, err := db.Query("UPDATE commits SET email=$2, date=$3, message=$4, additions=$5, deletions=$6, total=$7 WHERE id=$1", id, email, date, message, additions, deletions, total)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-}
-
-func shasDbQuery() bool {
-	rows, err := db.Query("SELECT id, repo, sha FROM commits WHERE total IS NULL LIMIT $1", *limit)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	next := false
-	for rows.Next() {
-		next = true
-		var id, repo, sha string
-		if err := rows.Scan(&id, &repo, &sha); err != nil {
+func dbQuery() {
+	for {
+		rows, err := db.Query("SELECT id, repo, sha FROM commits WHERE total IS NULL LIMIT $1", *limit)
+		if err != nil {
 			log.Fatal(err)
 		}
 
-		wg.Add(1)
-		go func(i, r, s string) {
-			defer wg.Done()
-			shas(i, r, s)
-		}(id, repo, sha)
-	}
+		if !rows.Next() {
+			break
+		}
 
-	return next
+		for rows.Next() {
+			var id, repo, sha string
+			if err := rows.Scan(&id, &repo, &sha); err != nil {
+				log.Fatal(err)
+			}
+
+			wg.Add(1)
+			go func(id, repo, sha string) {
+				defer wg.Done()
+				log.Printf("repo=%s sha=%s\n", repo, sha)
+				url := fmt.Sprintf("https://api.github.com/repos/%s/%s/commits/%s", org, repo, sha)
+				request(url, shas(id))
+			}(id, repo, sha)
+		}
+	}
 }
 
-func shasDbFind(repo, sha string) bool {
+func dbFind(repo, sha string) bool {
 	rows, err := db.Query("SELECT id FROM commits WHERE repo = $1 AND sha = $2", repo, sha)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer rows.Close()
 
 	return rows.Next()
 }
 
-func shasDbCreate(repo, sha string) {
-	rows, err := db.Query("INSERT INTO commits (id, repo, sha) VALUES ($1, $2, $3)", uuid.New(), repo, sha)
-	if err != nil {
+func dbCreate(repo, sha string) {
+	if _, err := db.Query("INSERT INTO commits (repo, sha) VALUES ($1, $2)", repo, sha); err != nil {
 		log.Fatal(err)
 	}
-	defer rows.Close()
 }
 
-func shasDb(repo, sha string) {
-	log.Printf("repo=%s sha=%s\n", repo, sha)
-	if !shasDbFind(repo, sha) {
-		shasDbCreate(repo, sha)
+func dbUpdate(id, email, date, message string, additions, deletions, total int) {
+	if _, err := db.Query("UPDATE commits SET email=$2, date=$3, msg=$4, adds=$5, dels=$6, total=$7 WHERE id=$1", id, email, date, message, additions, deletions, total); err != nil {
+		log.Fatal(err)
 	}
 }
 
-func commitsHandler(repo string) handler {
-	return func(rc io.ReadCloser) error {
+func commits(repo string) handler {
+	return func(rc io.ReadCloser) {
 		defer rc.Close()
 
 		var result []struct {
@@ -201,27 +176,22 @@ func commitsHandler(repo string) handler {
 		}
 
 		if err := json.NewDecoder(rc).Decode(&result); err != nil {
-			return err
+			log.Fatal(err)
 		}
 
 		for _, c := range result {
-			shasDb(repo, c.Sha)
+			func(repo, sha string) {
+				log.Printf("repo=%s sha=%s\n", repo, sha)
+				if !dbFind(repo, sha) {
+					dbCreate(repo, sha)
+				}
+			}(repo, c.Sha)
 		}
-
-		return nil
 	}
 }
 
-func commits(repo string) {
-	log.Printf("repo=%s\n", repo)
-	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/commits", org, repo)
-	if err := requester(url, commitsHandler(repo)); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func reposHandler() handler {
-	return func(rc io.ReadCloser) error {
+func repos() handler {
+	return func(rc io.ReadCloser) {
 		defer rc.Close()
 
 		var result []struct {
@@ -229,26 +199,22 @@ func reposHandler() handler {
 		}
 
 		if err := json.NewDecoder(rc).Decode(&result); err != nil {
-			return err
+			log.Fatal(err)
 		}
 
 		for _, r := range result {
 			wg.Add(1)
-			go func(r string) {
+			go func(repo string) {
 				defer wg.Done()
-				commits(r)
+				log.Printf("repo=%s\n", repo)
+				url := fmt.Sprintf("https://api.github.com/repos/%s/%s/commits", org, repo)
+				request(url, commits(repo))
 			}(r.Name)
 		}
-
-		return nil
 	}
 }
 
-func repos() {
-	url := fmt.Sprintf("https://api.github.com/orgs/%s/repos", org)
-	if err := requester(url, reposHandler()); err != nil {
-		log.Fatal(err)
-	}
+func dbSetup() {
 }
 
 func main() {
@@ -257,13 +223,25 @@ func main() {
 
 	flag.Parse()
 
+	if *setup {
+		dbSetup()
+	}
+
 	if *insert {
-		repos()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			url := fmt.Sprintf("https://api.github.com/orgs/%s/repos", org)
+			request(url, repos())
+		}()
 	}
 
 	if *update {
-		for shasDbQuery() {
-		}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			dbQuery()
+		}()
 	}
 
 	wg.Wait()
