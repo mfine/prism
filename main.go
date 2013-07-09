b2dec3be8eac99a6a770928f8343f006eaf32982
Mark Fine <mark.fine@gmail.com>
2013-07-09 13:29:29 -0400

    date filtering.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
 	"encoding/json"
 	"flag"
@@ -263,11 +264,32 @@ func commits(repo string) {
 	requests(commitsUrl(repo), commitsHandler(repo))
 }
 
+func dateCheck(created, pushed string) bool {
+	if *since != "" {
+		sinceBytes := bytes.NewBufferString(*since).Bytes()
+		pushedBytes := bytes.NewBufferString(pushed).Bytes()
+		if bytes.Compare(sinceBytes, pushedBytes) == 1 {
+			return false
+		}
+	}
+	if *until != "" {
+		createdBytes := bytes.NewBufferString(created).Bytes()
+		untilBytes := bytes.NewBufferString(*until).Bytes()
+		if bytes.Compare(createdBytes, untilBytes) == 1 {
+			return false
+		}
+	}
+
+	return true
+}
+
 // repos request processing
 func reposHandler(c chan<- func(), ignored map[string]bool) handler {
 	return func(rc io.Reader) {
 		var result []struct {
-			Name string
+			Name       string
+			Created_at string
+			Pushed_at  string
 		}
 
 		if err := json.NewDecoder(rc).Decode(&result); err != nil {
@@ -277,8 +299,8 @@ func reposHandler(c chan<- func(), ignored map[string]bool) handler {
 
 		// walk through repos, if not ignored add to worker
 		for _, r := range result {
-			log.Printf("fn=reposHandler org=%v repo=%v\n", *org, r.Name)
-			if !ignored[r.Name] {
+			log.Printf("fn=reposHandler org=%v repo=%v created=%q pushed=%q\n", *org, r.Name, r.Created_at, r.Pushed_at)
+			if !ignored[r.Name] && dateCheck(r.Created_at, r.Pushed_at) {
 				c <- func(repo string) func() { return func() { commits(repo) } }(r.Name)
 			}
 		}
